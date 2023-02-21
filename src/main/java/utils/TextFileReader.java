f94e0a3a79c33f864ae394b8973619cdd723a541
tranthanhlam10 <tranthanhlam85.TT@gmail.com>
2023-02-21 23:23:39 +0700

    Change env while running test, add readTextFile function.

---
+++ src/main/java/utils/TextFileReader.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TextFileReader {
+    private String file_path;
+
+    public void setFile_path(String file_path) {
+        this.file_path = file_path;
+    }
+    public String getFile_path(){
+        return this.file_path;
+    }
+
+    public String readTextFile(String file_path) throws FileNotFoundException {
+        File file = new File(file_path);
+        String diver = "";
+        Scanner read_file = new Scanner(file);
+        while (read_file.hasNextLine()) {
+            diver = read_file.nextLine();
+        }
+        read_file.close();
+        return  diver;
+    }
+}
--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -3,7 +3,9 @@ package pages;
 import drivermanager.BDriverFactory;
 import org.openqa.selenium.WebDriver;
 import utils.PropertiesFileReader;
+import utils.TextFileReader;
 
+import java.io.FileNotFoundException;
 import java.time.Duration;
 
 public class BasePage {
@@ -13,11 +15,16 @@ public class BasePage {
      * Hàm setup driver, test trang nào chỉ cần dán link trang đó vào
      * Ở đây demo nên tui lấy 1 trang sample
      */
-    public void setDriver(){
-//        driver = BDriverFactory.InitDriver(System.getProperty("driver"));
-          driver = BDriverFactory.InitDriver("chrome");
+    public void setDriver() throws FileNotFoundException {
+
+          TextFileReader fileReader = new TextFileReader();
+          fileReader.setFile_path("src/test/resources/config.txt");
+
+          String  env = (System.getProperty("env") == null) ? "testing" :System.getProperty("env");
+          driver = BDriverFactory.InitDriver(fileReader.readTextFile(fileReader.getFile_path()));
+          // Cần phải improve chỗ này, lỗi khi không thể set 2 properties cho Maven. Mai fix.
           driver.manage().window().maximize();
-          driver.navigate().to("https://eca-tool-testing.younetmedia.com/auth/login");
+          driver.navigate().to("https://eca-tool-"+env+".younetmedia.com/auth/login");
           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
       }
       public void tearDown(){
