a83d99af4f748b83bc8ab110ceed4545f4bf0e95
LamTT <tranthanhlam85.TT@gmail.com>
2025-07-03 18:18:42 +0700

    checkout b new

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -12,10 +12,7 @@ public class BasePage {
       public static WebDriver driver;
       public static string settingFilePath = "src/test/resources/config.txt";
 
-    /**
-     * Hàm setup driver, test trang nào chỉ cần dán link trang đó vào
-     * Ở đây demo nên tui lấy 1 trang sample
-     */
+ 
     public void setDriver() throws FileNotFoundException {
 
           TextFileReader fileReader = new TextFileReader();
@@ -23,7 +20,7 @@ public class BasePage {
 
           String  env = (System.getProperty("env") == null) ? "testing" :System.getProperty("env");
           driver = BDriverFactory.InitDriver(fileReader.readTextFile(fileReader.getFile_path()));
-          
+
           // Cần phải improve chỗ này, lỗi khi không thể set 2 properties cho Maven. Mai fix.
           driver.manage().window().maximize();
           driver.navigate().to("https://eca-tool-"+env+".younetmedia.com/auth/login");
@@ -33,9 +30,7 @@ public class BasePage {
           driver.quit();
       }
 
-    /**
-     * Hàm đọc file properties, có khi lại cần sử dụng đến
-     */
+    
     public void setupPropertiesFile(){
           PropertiesFileReader.setPropertiesFile();
      }
